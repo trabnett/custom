ec94315b3682bc131a56631f94eb74b4745df7b5
trabnett <timrabnett@gmail.com>
2019-01-10 20:26:05 +0000

    custom sorting with correct solution

--- custom.js
+++ custom.js
@@ -1,5 +1,16 @@
-arr = [10,5,2,9,1]
+var students = [
+ { id: 1, name: "bruce",    age: 40 },
+ { id: 2, name: "zoidberg", age: 22 },
+ { id: 3, name: "alex",     age: 22 },
+ { id: 4, name: "alex",     age: 30 }
 
-console.log(arr.sort(function(a,b){
-  return (a - b)
-  }))
+];
+
+results = students.sort(function(a,b) {
+ if(a.name > b.name) {
+   return a.name > b.name
+ } if(a.name === b.name) {
+   return b.age - a.age
+   }
+})
+console.log(results)
